23827f28a6130af83086c5b241e5b9673db45c93
Coltyn Gatton <c527g802@ku.edu>
2019-03-04 10:17:12 -0600

    Redirect to home page after login and login page works now.

--- greenprep/urls.py
+++ greenprep/urls.py
@@ -2,9 +2,16 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('meals/', include('mealbuilder.urls')),
     path('grocerylist/', include('grocerylist.urls')),
     path('profile/', include('userprofile.urls')),
 ]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
