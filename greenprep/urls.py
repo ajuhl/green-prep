e20d35eec9a39456e257c4e8185ebb1e531437db
ajuhl <a002j556@ku.edu>
2019-03-04 12:51:42 -0600

    Added static folder and favicon.

--- greenprep/urls.py
+++ greenprep/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mealbuilder/', include('mealbuilder.urls')),
+    path('meals/', include('mealbuilder.urls')),
     path('grocerylist/', include('grocerylist.urls')),
-    path('userprofile/', include('userprofile.urls')),
+    path('profile/', include('userprofile.urls')),
 ]
