e7b7aafe0b2edd8827092e2f9674438ab321f4f7
Jules Garrett <jrgarrett3222@gmail.com>
2019-04-24 13:38:39 -0500

    edited profile forms

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -5,4 +5,4 @@ from .models import Profile
 class UserForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('birthdate', 'sex', 'height', 'weight', 'activity_level', 'physical_goal')
+        fields = ('user','birthdate', 'sex', 'height', 'weight', 'activity_level', 'physical_goal')
